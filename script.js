7dbe95f252583855f311a80fbe6c56b81304a5f0
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-10-05 18:03:37 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+        const themeText = document.getElementById("themeText");
+        const currentTheme = localStorage.getItem("theme");
+
+        if (currentTheme) {
+            document.documentElement.setAttribute("data-theme", currentTheme);
+            themeText.innerText = (currentTheme === "dark") ? "Light Mode" : "Dark Mode"; // Update text based on stored theme.
+        }
+
+        darkModeToggle.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            let theme = "light";
+            if (document.documentElement.getAttribute("data-theme") === "light") {
+                theme = "dark";
+            }
+
+            document.documentElement.setAttribute("data-theme", theme);
+            localStorage.setItem("theme", theme);
+
+            // Toggle the text inside the <span>
+            themeText.innerText = (theme === "dark") ? "Light Mode" : "Dark Mode";
+        });
