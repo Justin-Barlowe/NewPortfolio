d03b3559d246bb98383160eb9d8c2ee3330caf43
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-03-06 19:05:20 -0500

    FIX: Formatting

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 // Dark Mode
 const darkModeToggle = document.getElementById("darkModeToggle");
-        const themeText = document.getElementById("themeText");
-        const currentTheme = localStorage.getItem("theme");
+const themeText = document.getElementById("themeText");
+const currentTheme = localStorage.getItem("theme");
 
-        if (currentTheme) {
-            document.documentElement.setAttribute("data-theme", currentTheme);
-            themeText.innerText = (currentTheme === "dark") ? "Light Mode" : "Dark Mode"; // Update text based on stored theme.
-        }
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  themeText.innerText = currentTheme === "dark" ? "Light Mode" : "Dark Mode"; // Update text based on stored theme.
+}
 
-        darkModeToggle.addEventListener("click", function (event) {
-            event.preventDefault();
+darkModeToggle.addEventListener("click", function (event) {
+  event.preventDefault();
 
-            let theme = "light";
-            if (document.documentElement.getAttribute("data-theme") === "light") {
-                theme = "dark";
-            }
+  let theme = "light";
+  if (document.documentElement.getAttribute("data-theme") === "light") {
+    theme = "dark";
+  }
 
-            document.documentElement.setAttribute("data-theme", theme);
-            localStorage.setItem("theme", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
 
-            // Toggle the text inside the <span>
-            themeText.innerText = (theme === "dark") ? "Light Mode" : "Dark Mode";
-        });
+  // Toggle the text inside the <span>
+  themeText.innerText = theme === "dark" ? "Light Mode" : "Dark Mode";
+});
 
-    
 // Hamburger Menu
-        document.querySelector('.hamburger').addEventListener('click', function() {
-            const menuItems = document.querySelector('nav .right');
-            if (menuItems.style.display === 'none' || menuItems.style.display === '') {
-                menuItems.style.display = 'flex';
-            } else {
-                menuItems.style.display = 'none';
-            }
-        });
-        
+document.querySelector(".hamburger").addEventListener("click", function () {
+  const menuItems = document.querySelector("nav .right");
+  if (menuItems.style.display === "none" || menuItems.style.display === "") {
+    menuItems.style.display = "flex";
+  } else {
+    menuItems.style.display = "none";
+  }
+});
